95ee31d43a03737c64dd79ac14466d6a079a4339
Mokou2005 <mokoou9@gmail.com>
2025-07-29 13:16:07 +0900

    script追加

---
+++ Assets/ActionSTG/Script/アイテム/ItemManager.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    //プレイヤーが持つアイテムリストをstringで保存
+     public List<string> itemList = new List<string>();
+    //アイテム追加
+    public void AddItem(string itemName)
+    {
+        if (!itemList.Contains(itemName))
+        {
+            itemList.Add(itemName);
+            Debug.Log("アイテムを追加しました: " + itemName);
+        }
+        else
+        {
+            Debug.Log("すでに持っています: " + itemName);
+        }
+    }
+    // アイテムを所持しているか確認
+    public bool HasItem(string itemName)
+    {
+        return itemList.Contains(itemName);
+    }
+}
